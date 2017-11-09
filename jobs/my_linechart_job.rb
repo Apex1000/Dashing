6a4deeb207b44dc18ecf1864c1030e0d6cc1205f
apex1000 <siddhartsingh9876@gmail.com>
2017-11-10 02:18:00 +0530

    added all data

--- jobs/my_linechart_job.rb
+++ jobs/my_linechart_job.rb
@@ -1,30 +1,30 @@
 require 'nokogiri'
 require 'open-uri'
-current_valuation = 0
+require "rest-client"
 
-labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+EARTHQUAKE_FEED_URL ="https://haha-4cf04.firebaseio.com/.json"
+# source = "https://haha-4cf04.firebaseio.com/.json"
 
-SCHEDULER.every '10s' do
-  doc = Nokogiri::HTML(open("http://www.cpcb.gov.in/caaqm/frmCurrentDataNew.aspx?StationName=Sidhu%20Kanhu%20Indoor%20Stadium&StateId=29&CityId=552"))
-  el = doc.xpath("//td[@id='Td1']").first
-  table = el.css('table')
-  c = table.css('tr')
-  c1 = c.css('td[4]')
-
-
-
-  last_valuation = current_valuation
-  haha = c1[2].text
-  current_valuation = haha.to_f
-  data = [
+labels = ['1','2','3','4','5','6','7','8','9']
+i=0
+SCHEDULER.every '10s', :first_in => '0' do |job|
+  eq_json_data = RestClient.get EARTHQUAKE_FEED_URL
+  raw_data = JSON.parse(eq_json_data)
+  real_data = []
+  raw_data['todos1'].each do |f|
+    sound = f['sound']
+    real_data << [sound]
+    end
+    # current_valuation=lat
+    data = [
     {
       label: 'First dataset',
-      data: Array.new(labels.length) { current_valuation },
+      data: Array.new(labels.length) { real_data[i] },
       backgroundColor: [ 'rgba(255, 99, 132, 0.2)' ] * labels.length,
       borderColor: [ 'rgba(255, 99, 132, 1)' ] * labels.length,
       borderWidth: 1,
     }
   ]
-
-  send_event('linechart', { labels: labels, datasets: data })
+    send_event('linechart', { labels: labels, datasets: data })
+i=i+1
 end
